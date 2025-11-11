8c6458b1113580a8432abf9b881823428ca8f5cd
Adit <adityaekaw21@gmail.com>
2025-11-11 14:31:22 +0700

    feat: adding Profile card

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@
  * @license Apache-2.0 className="0"></Apache-2>
  */
 
-import { Button } from '@/components/ui/button';
+/**
+ * Node modules
+ */
+import { useEffect } from 'react';
+
+/**
+ * Components
+ */
 
 export const App = () => {
-  return <Button>Click Me</Button>;
+  return (
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'></main>
+  );
 };
