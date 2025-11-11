f6179f88456e426562a05992b503dbfe4c6cae89
Adit <adityaekaw21@gmail.com>
2025-11-11 16:41:15 +0700

    feat: adding Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@
  * Node modules
  */
 import { useEffect } from 'react';
+import { Hero } from '@/components/Hero';
 
 /**
  * Components
@@ -14,6 +15,8 @@ import { useEffect } from 'react';
 
 export const App = () => {
   return (
-    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'></main>
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
+      <Hero />
+    </main>
   );
 };
