b928320662d58ccb7593258dc91b9f31e9607769
Adit <adityaekaw21@gmail.com>
2025-11-12 14:25:39 +0700

    feat: adding Status section and change on stat index

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@
  */
 import { useEffect } from 'react';
 import { Hero } from '@/components/Hero';
+import { Stats } from '@/components/Stats';
 
 /**
  * Components
@@ -17,6 +18,7 @@ export const App = () => {
   return (
     <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
       <Hero />
+      <Stats />
     </main>
   );
 };
